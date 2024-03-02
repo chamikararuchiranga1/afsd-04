3a6546f11099b0adb2727ae2e5aaf202de425479
chami <chamikararuchiranga1@gmail.com>
2024-03-02 17:04:27 +0530

    assign variable

---
+++ JavaScript/index.js
@@ -0,0 +1,52 @@
+console.log('ACPT')
+
+document.write('<h1>Java Script</h1>')
+document.write('Chamikara Ruchiranga')
+document.write('<h3>ACPT</h3>')
+document.write("<button>Click me</button>")
+
+// alert('Chamikara')
+
+// Number 
+const number = 10.5
+
+// String 
+const string = 'acpt';
+
+// Boolean 
+const bool = true;
+
+// Object 
+const object = {name: "chamikara", age: 28}
+
+// Array 
+const array = [{name: "chamikara", age: 28}, 12, true, "Chamikara"];
+
+// var , let , const 
+
+// var 
+var test = 10;
+console.log(test)
+
+{var test = 20}
+console.log(test)
+test = 100;
+console.log(test);
+
+// let 
+let sample = 200;
+console.log(sample);
+
+// let sample = 300
+sample = 500;
+console.log(sample);
+
+{let sample1 = 350}
+// console.log(sample1)
+
+// const 
+{const acpt = 1000;}
+const acpt = 3000;
+console.log(acpt);
+// const acpt = 2000;
+// acpt = 2000;
